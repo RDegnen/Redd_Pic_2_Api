b0f45c9cac10225f9cbd56e17371fcbd5d4dbc5d
Ross <rdegnen@gmail.com>
2015-04-02 09:41:31 -0400

    can retrieve other subreddits

--- app/controllers/imgurs_controller.rb
+++ app/controllers/imgurs_controller.rb
@@ -1,15 +1,18 @@
 class ImgursController < ApplicationController
+  # attr_accessor :request
 
   def index
     # # parse the params hash
     # { "subreddit" => "nakedladytees" }
     # params["subreddit"] # => nakedladytees
-    @response = RestClient.get("https://api.imgur.com/3/gallery/r/#{@request}", Authorization: "Client-ID #{ENV['IMGUR_CLIENT_ID']}")
+    @response = RestClient.get("https://api.imgur.com/3/gallery/r/pics", Authorization: "Client-ID #{ENV['IMGUR_CLIENT_ID']}")
     render json: @response, status: 200
   end
 
   def create
     @request = params[:name]
+    @response = RestClient.get("https://api.imgur.com/3/gallery/r/#{@request}", Authorization: "Client-ID #{ENV['IMGUR_CLIENT_ID']}")
+    render json: @response, status: 200
   end
 
 end
