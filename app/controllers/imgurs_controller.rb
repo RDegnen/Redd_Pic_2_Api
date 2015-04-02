ea96d3cc0d46256027c40551bedc969ae7d235c0
Ross <rdegnen@gmail.com>
2015-04-02 11:42:05 -0400

    removed comments

--- app/controllers/imgurs_controller.rb
+++ app/controllers/imgurs_controller.rb
@@ -1,10 +1,6 @@
 class ImgursController < ApplicationController
-  # attr_accessor :request
 
   def index
-    # # parse the params hash
-    # { "subreddit" => "nakedladytees" }
-    # params["subreddit"] # => nakedladytees
     @response = RestClient.get("https://api.imgur.com/3/gallery/r/pics", Authorization: "Client-ID #{ENV['IMGUR_CLIENT_ID']}")
     render json: @response, status: 200
   end
