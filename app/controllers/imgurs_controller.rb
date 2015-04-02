714e541b6022f534d22aa52844754f9588a73499
Ross <rdegnen@gmail.com>
2015-04-01 22:45:45 -0400

    recieveing request from front end

--- app/controllers/imgurs_controller.rb
+++ app/controllers/imgurs_controller.rb
@@ -1,8 +1,15 @@
 class ImgursController < ApplicationController
 
   def index
-    @response = RestClient.get('https://api.imgur.com/3/gallery/r/pics/', Authorization: "Client-ID #{ENV['IMGUR_CLIENT_ID']}")
-
+    # # parse the params hash
+    # { "subreddit" => "nakedladytees" }
+    # params["subreddit"] # => nakedladytees
+    @response = RestClient.get("https://api.imgur.com/3/gallery/r/#{@request}", Authorization: "Client-ID #{ENV['IMGUR_CLIENT_ID']}")
     render json: @response, status: 200
   end
+
+  def create
+    @request = params[:name]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :imgurs, only: [:index]
+  resources :imgurs, only: [:index, :create]
 end
