5c2276ae4c657d2d9f0f9a61495755f00f7680d8
Ross <rdegnen@gmail.com>
2015-04-09 10:12:10 -0400

    added line to try and deploy to heroku

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module ReddPicApi
         resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
       end
      end
+
+     config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
